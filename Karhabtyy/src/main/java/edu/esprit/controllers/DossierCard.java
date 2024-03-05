f1c95051e405341c0b522ca92ae1c42ce7cef172
20eva <yefa.attia@esprit.tn>
2024-03-05 14:29:30 +0100

    api

---
+++ Karhabtyy/src/main/java/edu/esprit/controllers/DossierCard.java
@@ -0,0 +1,34 @@
+package edu.esprit.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class Dossier {
+
+    @FXML
+    private Button buttadd;
+
+    @FXML
+    private Button buttdel;
+
+    @FXML
+    private TextField fxx2name;
+
+    @FXML
+    private TextField fxxcin;
+
+    @FXML
+    private DatePicker fxxdate;
+
+    @FXML
+    private TextField fxxmontant;
+
+    @FXML
+    private TextField fxxname;
+
+    @FXML
+    private TextField fxxregion;
+
+}
